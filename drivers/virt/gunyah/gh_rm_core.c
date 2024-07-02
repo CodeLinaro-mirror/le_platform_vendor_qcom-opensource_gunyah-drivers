4502cd24791f0254361a8a62e2359b8d1a4ec65a
Wei Liu <quic_wliu8@quicinc.com>
2024-07-02 15:47:53 +0800

    drivers: virt: gunyah: add autogvmlv support
    Add autogvmlv GVM definition.
    
    Change-Id: I1ab659e99c59733b058c8ac1c43aefec0bbec603
    Signed-off-by: Wei Liu <quic_wliu8@quicinc.com>

--- drivers/virt/gunyah/gh_rm_core.c
+++ drivers/virt/gunyah/gh_rm_core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  */
 
@@ -78,6 +78,7 @@ const static struct {
 	{GH_CPUSYS_VM, "cpusys_vm", "qcom,cpusysvm"},
 	{GH_OEM_VM, "oem_vm", "qcom,oemvm"},
 	{GH_AUTO_VM, "autoghgvm", "qcom,autoghgvm"},
+	{GH_AUTO_VM_LV, "autoghgvmlv", "qcom,autoghgvmlv"},
 };
 
 static struct task_struct *gh_rm_drv_recv_task;

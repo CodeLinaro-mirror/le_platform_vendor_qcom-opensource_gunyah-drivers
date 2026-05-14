2fdb9fc71db293a4e5767d048a38e9528f11f19d
Ramachandran Venkataramani <ramavenk@qti.qualcomm.com>
2026-05-14 13:30:53 -0700

    gunyah-drivers: Add timeouts for wait for vm status
    Change gh_wait_for_vm_status to use wait with timeout.
    Handle errors when the wait times out for these calls.
    
    Change-Id: If478bbe8620b0fa1accd457c7547e12c6439b2eb
    (cherry picked from commit be01c23c2ea24352490ff8f7094150b2c428a76d)

--- drivers/virt/gunyah/gh_main.c
+++ drivers/virt/gunyah/gh_main.c
@@ -27,6 +27,7 @@
 
 #define MAX_VCPU_NAME		20 /* gh-vcpu:u32_max +1 */
 #define MAX_VMID			128
+#define GH_VM_STATUS_WAIT_TIMEOUT	msecs_to_jiffies(5000) /* 5 seconds */
 
 
 SRCU_NOTIFIER_HEAD_STATIC(gh_vm_notifier);
@@ -37,6 +38,9 @@ static struct gh_shiomem_info shiomem_info = {
 	.slock = __SPIN_LOCK_UNLOCKED(shiomem_info.slock),
 };
 
+static inline void gh_vm_set_status(struct gh_vm *vm, u8 status);
+static inline u8 gh_vm_get_status(struct gh_vm *vm);
+
 /*
  * Support for RM calls and the wait for change of status
  */
@@ -46,14 +50,29 @@ static int gh_##name(struct gh_vm *vm, int vm_status)			 \
 	int ret = 0;							 \
 	ret = gh_rm_##name(vm->vmid);					 \
 	if (!ret)							 \
-		vm->status.vm_status = vm_status;			 \
+		gh_vm_set_status(vm, vm_status);			 \
 	return ret;							 \
 }
 
 gh_rm_call_and_set_status(vm_start);
 
-#define gh_wait_for_vm_status(vm, wait_status) 				\
-	wait_event(vm->vm_status_wait, (vm->status.vm_status == wait_status))
+static inline u8 gh_vm_get_status(struct gh_vm *vm)
+{
+	return READ_ONCE(vm->status.vm_status);
+}
+
+static inline void gh_vm_set_status(struct gh_vm *vm, u8 status)
+{
+	WRITE_ONCE(vm->status.vm_status, status);
+}
+
+static inline int gh_wait_for_vm_status(struct gh_vm *vm, int wait_status,
+					unsigned long timeout)
+{
+	return wait_event_timeout(vm->vm_status_wait,
+			READ_ONCE(vm->status.vm_status) == wait_status,
+			timeout) ? 0 : -ETIMEDOUT;
+}
 
 static struct gh_vm *find_vm_by_name(const char *vm_name)
 {
@@ -127,12 +146,12 @@ static void gh_notif_vm_status(struct gh_vm *vm,
 		return;
 
 	/* Wake up the waiters only if there's a change in any of the states */
-	if (status->vm_status != vm->status.vm_status &&
+	if (status->vm_status != gh_vm_get_status(vm) &&
 	   (status->vm_status == GH_RM_VM_STATUS_RESET ||
 	   status->vm_status == GH_RM_VM_STATUS_READY)) {
 		pr_info("VM: %d status %d complete\n", vm->vmid,
 							status->vm_status);
-		vm->status.vm_status = status->vm_status;
+		gh_vm_set_status(vm, status->vm_status);
 		wake_up(&vm->vm_status_wait);
 	}
 }
@@ -145,7 +164,7 @@ static void gh_notif_vm_exited(struct gh_vm *vm,
 
 	mutex_lock(&vm->vm_lock);
 	vm->exit_type = vm_exited->exit_type;
-	vm->status.vm_status = GH_RM_VM_STATUS_EXITED;
+	gh_vm_set_status(vm, GH_RM_VM_STATUS_EXITED);
 	gh_wakeup_all_vcpus(vm->vmid);
 	wake_up(&vm->vm_status_wait);
 	wake_up_interruptible(&vm->vm_status_wait);
@@ -158,7 +177,7 @@ static int gh_wait_for_vm_status_intr(struct gh_vm *vm, int wait_status)
 	int ret = 0;
 
 	ret = wait_event_freezable(vm->vm_status_wait,
-			vm->status.vm_status == wait_status);
+			gh_vm_get_status(vm) == wait_status);
 	if (ret < 0)
 		pr_err("Wait for VM_STATUS %d interrupted\n", wait_status);
 
@@ -170,7 +189,7 @@ static int gh_ioc_wait_for_vm_status(struct gh_vm *vm, int wait_status)
 	int ret = 0;
 
 	ret = wait_event_freezable(vm->vm_exit_ioc_wait,
-			vm->status.vm_status == wait_status);
+			gh_vm_get_status(vm) == wait_status);
 	if (ret < 0)
 		pr_err("Wait for VM_STATUS %d interrupted\n", wait_status);
 
@@ -199,7 +218,7 @@ static int gh_vm_rm_notifier_fn(struct notifier_block *nb,
 static void gh_vm_cleanup(struct gh_vm *vm)
 {
 	gh_vmid_t vmid = vm->vmid;
-	int vm_status = vm->status.vm_status;
+	int vm_status = gh_vm_get_status(vm);
 	int ret;
 
 	switch (vm_status) {
@@ -213,7 +232,10 @@ static void gh_vm_cleanup(struct gh_vm *vm)
 	case GH_RM_VM_STATUS_AUTH:
 		ret = gh_rm_vm_reset(vmid);
 		if (!ret) {
-			gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_RESET);
+			ret = gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_RESET,
+						   GH_VM_STATUS_WAIT_TIMEOUT);
+			if (ret)
+				pr_warn("Wait for VM:%d reset status failed: %d\n", vmid, ret);
 		} else
 			pr_warn("Reset is unsuccessful for VM:%d\n", vmid);
 
@@ -232,19 +254,19 @@ static void gh_vm_cleanup(struct gh_vm *vm)
 			pr_warn("Failed to dealloc VMID: %d: %d\n", vmid, ret);
 	}
 
-	vm->status.vm_status = GH_RM_VM_STATUS_NO_STATE;
+	gh_vm_set_status(vm, GH_RM_VM_STATUS_NO_STATE);
 }
 
 static int gh_exit_vm(struct gh_vm *vm, u32 stop_reason, u8 stop_flags)
 {
 	gh_vmid_t vmid = vm->vmid;
-	int ret = -EINVAL;
+	int ret;
 
 	if (!vmid)
 		return -ENODEV;
 
 	mutex_lock(&vm->vm_lock);
-	if (vm->status.vm_status != GH_RM_VM_STATUS_RUNNING) {
+	if (gh_vm_get_status(vm) != GH_RM_VM_STATUS_RUNNING) {
 		pr_err("VM:%d is not running\n", vmid);
 		mutex_unlock(&vm->vm_lock);
 		return -ENODEV;
@@ -258,9 +280,14 @@ static int gh_exit_vm(struct gh_vm *vm, u32 stop_reason, u8 stop_flags)
 	}
 	mutex_unlock(&vm->vm_lock);
 
-	gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_EXITED);
+	ret = gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_EXITED,
+				   GH_VM_STATUS_WAIT_TIMEOUT);
+	if (ret) {
+		pr_err("Failed to wait for VM:%d exit status: %d\n", vmid, ret);
+		return ret;
+	}
 
-	return ret;
+	return 0;
 }
 
 static int gh_stop_vm(struct gh_vm *vm)
@@ -297,7 +324,7 @@ void gh_destroy_vm(struct gh_vm *vm)
 {
 	int vcpu_id = 0;
 
-	if (vm->status.vm_status == GH_RM_VM_STATUS_NO_STATE)
+	if (gh_vm_get_status(vm) == GH_RM_VM_STATUS_NO_STATE)
 		goto clean_vm;
 
 	gh_stop_vm(vm);
@@ -352,13 +379,13 @@ static int gh_vcpu_ioctl_run(struct gh_vcpu *vcpu)
 
 	mutex_lock(&vm->vm_lock);
 
-	if (vm->status.vm_status == GH_RM_VM_STATUS_RUNNING) {
+	if (gh_vm_get_status(vm) == GH_RM_VM_STATUS_RUNNING) {
 		mutex_unlock(&vm->vm_lock);
 		goto start_vcpu_run;
 	}
 
 	if (vm->vm_run_once &&
-		vm->status.vm_status != GH_RM_VM_STATUS_RUNNING) {
+		gh_vm_get_status(vm) != GH_RM_VM_STATUS_RUNNING) {
 		pr_err("VM:%d has failed to run before\n", vm->vmid);
 		mutex_unlock(&vm->vm_lock);
 		return -EINVAL;
@@ -376,7 +403,7 @@ static int gh_vcpu_ioctl_run(struct gh_vcpu *vcpu)
 		return ret;
 	}
 
-	if (vm->status.vm_status != GH_RM_VM_STATUS_READY) {
+	if (gh_vm_get_status(vm) != GH_RM_VM_STATUS_READY) {
 		pr_err("VM:%d not ready to start\n", vm->vmid);
 		ret = -EINVAL;
 		mutex_unlock(&vm->vm_lock);
@@ -453,7 +480,7 @@ static int gh_vm_ioctl_get_vcpu_count(struct gh_vm *vm)
 	if (!vm->is_secure_vm)
 		return -EINVAL;
 
-	if (vm->status.vm_status != GH_RM_VM_STATUS_READY)
+	if (gh_vm_get_status(vm) != GH_RM_VM_STATUS_READY)
 		return -EAGAIN;
 
 	return vm->allowed_vcpus;
@@ -991,7 +1018,7 @@ long gh_vm_configure(u16 auth_mech, u64 image_offset,
 						vm->vmid, ret);
 			return ret;
 		}
-		vm->status.vm_status = GH_RM_VM_STATUS_AUTH;
+		gh_vm_set_status(vm, GH_RM_VM_STATUS_AUTH);
 		if (!pas_id) {
 			pr_err("Incorrect pas_id %d for VM:%d\n", pas_id,
 						vm->vmid);
@@ -1006,7 +1033,7 @@ long gh_vm_configure(u16 auth_mech, u64 image_offset,
 						vm->vmid, ret);
 			return ret;
 		}
-		vm->status.vm_status = GH_RM_VM_STATUS_INIT;
+		gh_vm_set_status(vm, GH_RM_VM_STATUS_INIT);
 		break;
 	default:
 		pr_err("Invalid auth mechanism for VM\n");
@@ -1020,7 +1047,12 @@ long gh_vm_configure(u16 auth_mech, u64 image_offset,
 		return ret;
 	}
 
-	gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_READY);
+	ret = gh_wait_for_vm_status(vm, GH_RM_VM_STATUS_READY,
+				   GH_VM_STATUS_WAIT_TIMEOUT);
+	if (ret) {
+		pr_err("Failed to wait for VM:%d ready status: %d\n", vm->vmid, ret);
+		return ret;
+	}
 
 	ret = gh_rm_populate_hyp_res(vm->vmid, fw_name);
 	if (ret < 0) {
@@ -1163,7 +1195,7 @@ static struct gh_vm *gh_create_vm(void)
 	refcount_set(&vm->users_count, 1);
 	init_waitqueue_head(&vm->vm_status_wait);
 	init_waitqueue_head(&vm->vm_exit_ioc_wait);
-	vm->status.vm_status = GH_RM_VM_STATUS_NO_STATE;
+	gh_vm_set_status(vm, GH_RM_VM_STATUS_NO_STATE);
 	vm->exit_type = -EINVAL;
 	vm->susp_irq = -EINVAL;
 	vm->vm_suspend_type = VM_STATE_CREATED;
